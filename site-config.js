f14a54652239e6f652c44abc71668dfe496c92e8
Adam <ablazek@securityscorecard.io>
2020-07-07 12:13:14 +0200

    update site config

--- site-config.js
+++ site-config.js
@@ -3,12 +3,12 @@ const path = require('path');
 module.exports = {
   siteTitle: `Svatba Aničky a Adama 💑`,
   siteTitleShort: `SvatbaAA`,
-  siteDescription: `Stranky venovane svatbe Anicky a Adama`,
+  siteDescription: `Stránky věnované svatbě Aničky a Adama`,
   siteUrl: `https://anickaadam.cz`,
   themeColor: `#F08080`,
   backgroundColor: `#F08080`,
   pathPrefix: null,
-  logo: path.resolve(__dirname, 'src/images/sparkles.png'),
+  logo: path.resolve(__dirname, 'src/images/icon.png'),
   social: {
     twitter: `gatsbyjs`,
     fbAppId: `966242223397117`,
