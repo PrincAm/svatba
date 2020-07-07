e77c7aa40baa2b5d49789e786207a49a12c66c5a
Adam <ablazek@securityscorecard.io>
2020-07-07 12:30:27 +0200

    update  social image and sitemap path

--- site-config.js
+++ site-config.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 module.exports = {
   siteTitle: `Svatba Aničky a Adama 💑`,
-  siteTitleShort: `SvatbaAA`,
+  siteTitleShort: `Svatba A&A`,
   siteDescription: `Stránky věnované svatbě Aničky a Adama`,
   siteUrl: `https://anickaadam.cz`,
   themeColor: `#F08080`,
   backgroundColor: `#F08080`,
   pathPrefix: null,
-  logo: path.resolve(__dirname, 'src/images/icon.png'),
+  logo: path.resolve(__dirname, 'src/images/wedding.png'),
   social: {
     twitter: `gatsbyjs`,
     fbAppId: `966242223397117`,
